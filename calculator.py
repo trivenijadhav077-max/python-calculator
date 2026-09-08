1038d22ead8cb425a5ca2763ea6539a0606864e4
trivenijadhav077-max <trivenijadhav077@gmail.com>
2026-09-08 22:44:24 +0530

    Add calculator.py

---
+++ calculator.py
@@ -0,0 +1,16 @@
+
+
+
+
+# Project #2 (Concept: Operators + Input/Print)
+# This project will create a simple calculator that performs addition, subtraction, multiplication, and division.
+num1=float(input("enter first number: "))
+num2=float(input("enter second number: "))
+Additional=num1+num2
+difference=num1-num2
+product=num1*num2
+division=num1/num2
+print("the sum of",num1,"and",num2,"is:",Additional)
+print("the difference of",num1,"and",num2,"is:",difference)
+print("the product of",num1,"and",num2,"is:",product)
+print("the division of",num1,"and",num2,"is:",division)
