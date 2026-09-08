6b778bd0d348e382313b5dda0d84d7483dbba37e
trivenijadhav077-max <trivenijadhav077@gmail.com>
2026-09-08 23:00:22 +0530

    Add zero division check

--- calculator.py
+++ calculator.py
@@ -2,15 +2,19 @@
 
 
 
-# Project #2 (Concept: Operators + Input/Print)
-# This project will create a simple calculator that performs addition, subtraction, multiplication, and division.
-num1=float(input("enter first number: "))
-num2=float(input("enter second number: "))
-Additional=num1+num2
-difference=num1-num2
-product=num1*num2
-division=num1/num2
-print("the sum of",num1,"and",num2,"is:",Additional)
-print("the difference of",num1,"and",num2,"is:",difference)
-print("the product of",num1,"and",num2,"is:",product)
-print("the division of",num1,"and",num2,"is:",division)
+num1 = float(input("enter first number: "))
+num2 = float(input("enter second number: "))
+
+Additional = num1 + num2
+difference = num1 - num2
+product = num1 * num2
+
+print("the sum of", num1, "and", num2, "is:", Additional)
+print("the difference of", num1, "and", num2, "is:", difference)
+print("the product of", num1, "and", num2, "is:", product)
+
+if num2 == 0:
+    print("division by zero is not possible")
+else:
+    division = num1 / num2
+    print("the division of", num1, "and", num2, "is:", division)
